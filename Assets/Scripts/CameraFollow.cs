20b5f8287052e2f4bf6f5ce5137cffe34b02163d
KozakDmitry <dimakaza4ka@gmail.com>
2023-09-05 14:51:16 +0300

    Rescaled

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,12 +15,12 @@ public class CameraFollow : MonoBehaviour
     public void SetPlayer(Transform player)
     {
         this.player = player;
-        transform.position = player.position + Vector3.forward * -10f;
+        transform.position = player.position + Vector3.forward * -20f;
     }
 
     private void FixedUpdate()
     {
-        Vector3 targetPosition = player.position + Vector3.forward * -10f;
+        Vector3 targetPosition = player.position + Vector3.forward * -20;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, 1f);
         transform.rotation = Quaternion.Euler(Vector3.zero);
         transform.position = smoothedPosition;
